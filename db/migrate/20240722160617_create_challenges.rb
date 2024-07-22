a0ecba4f7202cfd1a93e346c659331b2cb1265f7
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-23 00:11:41 +0800

    feat: add challenges table

---
+++ db/migrate/20240722160617_create_challenges.rb
@@ -0,0 +1,13 @@
+class CreateChallenges < ActiveRecord::Migration[7.1]
+  def change
+    create_table :challenges do |t|
+      # NOTE: Index title?
+      t.string :title, null: false
+      t.text :description, null: false
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/challenges.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :challenge do
+    title { "MyString" }
+    description { "MyText" }
+    start_date { "2024-07-23" }
+    end_date { "2024-07-23" }
+  end
+end

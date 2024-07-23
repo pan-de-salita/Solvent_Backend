e3f5f501b4406ab42f6cf8c80233c61ad8ab5288
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-23 15:29:37 +0800

    test: add Challenge model tests

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,6 +3,6 @@
 class Challenge < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :start_date, presence: true, comparison: { greater_than_or_equal_to: Date.today }
+  validates :end_date, presence: true, comparison: { greater_than: Date.today }
 end
--- spec/factories/challenges.rb
+++ spec/factories/challenges.rb
@@ -1,8 +1,19 @@
 FactoryBot.define do
   factory :challenge do
-    title { "MyString" }
-    description { "MyText" }
-    start_date { "2024-07-23" }
-    end_date { "2024-07-23" }
+    trait :with_title do
+      title { 'Test Challenge' }
+    end
+
+    trait :with_description do
+      description { 'Description for Test Challenge' }
+    end
+
+    trait :with_start_date do
+      start_date { Date.today }
+    end
+
+    trait :with_end_date do
+      end_date { Date.today + 10 }
+    end
   end
 end

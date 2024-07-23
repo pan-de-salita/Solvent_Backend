a3ad1523760ca98f41d1b8c8d497cfc8cec0c93f
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-23 15:51:41 +0800

    test: add update tests to Challenge model

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -13,30 +13,43 @@ RSpec.describe Challenge, type: :model do
   end
 
   context 'when attributes are invalid/missing' do
-    it 'rejects a Challenge instance creation attempt when title is not unique' do
+    it 'rejects an instance creation attempt when title is not unique' do
       challenge_with_duplicate_title = challenge.dup
       challenge.save
       expect(challenge_with_duplicate_title.title).to eq(challenge.title)
       expect(challenge_with_duplicate_title).to_not be_valid
     end
 
-    it 'rejects a Challenge instance creation attempt when start_date is less than Date.today' do
+    it 'rejects an creation attempt when start_date is less than Date.today' do
       challenge.start_date = Date.yesterday
       challenge_with_invalid_start_date = challenge
       expect(challenge_with_invalid_start_date.start_date).to be < Date.today
       expect(challenge_with_invalid_start_date).to_not be_valid
     end
 
-    it 'rejects a Challenge instance creation attempt when end_date is less than or equal_to Date.today' do
-      challenge.end_date = Date.today
-      challenge_with_invalid_end_date = challenge
-      expect(challenge_with_invalid_end_date.end_date).to be == challenge.start_date
-      expect(challenge_with_invalid_end_date).to_not be_valid
-
+    it 'rejects an instance creation attempt when end_date is less than or equal_to Date.today' do
       challenge.end_date = Date.yesterday
       challenge_with_invalid_end_date = challenge
-      expect(challenge_with_invalid_end_date.end_date).to be == Date.yesterday
+      expect(challenge_with_invalid_end_date.end_date).to be < challenge_with_invalid_end_date.start_date
       expect(challenge_with_invalid_end_date).to_not be_valid
+
+      challenge.end_date = Date.today
+      challenge_with_invalid_end_date = challenge
+      expect(challenge_with_invalid_end_date.end_date).to be == Date.today
+      expect(challenge_with_invalid_end_date).to_not be_valid
+    end
+  end
+
+  context 'when updating start/end_date attributes' do
+    let!(:saved_challenge) { create :challenge, :with_title, :with_description, :with_start_date, :with_end_date }
+
+    it 'rejects a instance update attempt when start_date is less than Date.today' do
+      expect(saved_challenge.update(start_date: Date.yesterday)).to be_falsy
+    end
+
+    it 'rejects a Challenge instance creation attempt when end_date is less than or equal_to Date.today' do
+      expect(saved_challenge.update(end_date: Date.yesterday)).to be_falsy
+      expect(saved_challenge.update(end_date: Date.today)).to be_falsy
     end
   end
 end

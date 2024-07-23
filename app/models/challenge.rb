e409d6b0daa77373ac60346de09e90d6fbb02835
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-23 14:06:15 +0800

    feat: add challenge controller

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Challenge < ApplicationRecord
-  validates :title, presence: true
-  validates :descrition, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
 end

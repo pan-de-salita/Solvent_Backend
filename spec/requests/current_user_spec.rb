133d52acf474ad400c26e58e0caefcde4ec04046
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-25 00:45:31 +0800

    feat: add request spec for CurrentUserController

--- spec/requests/current_user_spec.rb
+++ spec/requests/current_user_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
+require 'devise/jwt/test_helpers'
 
-RSpec.describe "CurrentUsers", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/current_user/index"
+RSpec.describe 'CurrentUsers', type: :request do
+  before do
+    # TODO: change when User model is updated.
+    @user = User.create(
+      email: 'test@mail.com',
+      password: 'foobar',
+      password_confirmation: 'foobar',
+      jti: SecureRandom.uuid
+    )
+  end
+
+  describe 'GET /index' do
+    it 'returns http success' do
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+      auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, @user)
+      get '/api/v1/auth/current_user', headers: auth_headers
       expect(response).to have_http_status(:success)
     end
   end
-
 end

8ac66aa83e7d1d0c20775889eb3b94158a84b6be
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-25 01:43:25 +0800

    feat: add Language model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_173347) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_174219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "puzzles", force: :cascade do |t|
     t.string "title", null: false

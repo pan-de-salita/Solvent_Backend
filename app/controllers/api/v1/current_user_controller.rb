d78d6c5aa8ede72ceb38e9714a8c82d171609642
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-25 00:07:32 +0800

    refactor: adjust CurrentUserController

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -6,7 +6,7 @@ module Api
       before_action :authenticate_user!
 
       def index
-        render json: ::UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+        render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
       end
     end
   end

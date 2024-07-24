3e4e6a14bf15fd719eaed16b514cd8c9228794a5
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-25 00:12:27 +0800

    refactor: adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'health_check', to: 'health_checks#index'
+      get '/auth/current_user', to: 'current_user#index'
 
       resources :puzzles
-      get '/current_user', to: 'current_user#index'
     end
   end
 end

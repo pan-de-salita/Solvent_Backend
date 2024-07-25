d6e8b5544225dcdf5083f2ac7f55705c443c661a
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-25 12:27:15 +0800

    refactor: user let instead of before do in curernt_user spec

--- spec/requests/current_user_spec.rb
+++ spec/requests/current_user_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 require 'devise/jwt/test_helpers'
 
 RSpec.describe 'CurrentUsers', type: :request do
-  before do
-    # TODO: change when User model is updated.
-    @user = create :user
-  end
+  let!(:user) { create(:user) }
 
   describe 'GET /index' do
     it 'returns http success' do
       headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
-      auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, @user)
+      auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
       get '/api/v1/auth/current_user', headers: auth_headers
       expect(response).to have_http_status(:success)
     end

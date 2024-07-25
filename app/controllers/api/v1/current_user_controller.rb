a15fd1e36b0b6666a06cc6c8ddbbc26ca9cec26d
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-26 03:04:37 +0800

    chore: add route comment to current_user controller

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -5,6 +5,7 @@ module Api
     class CurrentUserController < ApplicationController
       before_action :authenticate_user!
 
+      # GET api/v1/auth/current_user
       def index
         render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
       end

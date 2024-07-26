a9ce800e91b8b385f6b3c5d73c94ce2f293078cc
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-26 14:21:01 +0800

    fix: fix through options of User model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,12 @@ class User < ApplicationRecord
 
   has_many :puzzles, foreign_key: :creator_id
   has_many :solutions
-  has_many :languages, through: solutions
-  has_many :coauthored_solutions, through: solution_coauthors
+  has_many :languages, through: :solutions
+  has_many :coauthored_solutions, through: :solution_coauthors
   has_many :follows, foreign_key: 'follower_id'
-  has_many :followed_users, class_name: 'User', foreign_key: 'follower_id'
-  has_many :followers, class_name: 'User', foreign_key: 'followed_user_id'
+  has_many :followed_users, class_name: 'User', foreign_key: :follower_id
+  has_many :followers, class_name: 'User', foreign_key: :followed_user_id
   has_many :comments, through: :solutions
   has_many :likes
-  has_many :favorite_puzzles, through: puzzle_favorites
+  has_many :favorite_puzzles, through: :puzzle_favorites
 end

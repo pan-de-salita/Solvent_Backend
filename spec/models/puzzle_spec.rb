230e98f1004a639234f22d54fe521c1b5d8e6d24
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-26 14:26:41 +0800

    refactor: remove @puzzle instance variable from puzzle model spec

--- spec/models/puzzle_spec.rb
+++ spec/models/puzzle_spec.rb
@@ -18,34 +18,30 @@ require 'rails_helper'
 RSpec.describe Puzzle, type: :model do
   let!(:user) { create :user }
   let!(:language) { create :language }
-  let(:puzzle) { build :puzzle, title: 'Not the same as @puzzle' }
-
-  before do
-    @puzzle = build :puzzle
-    @puzzle.update(language_id: language.id, creator_id: user.id)
-  end
+  let(:puzzle) { build :puzzle, language_id: language.id, creator_id: user.id }
 
   context 'when attributes are valid' do
     it 'creates a Puzzle instance' do
-      expect(@puzzle).to be_valid
+      expect(puzzle).to be_valid
     end
   end
 
   context 'when title is not unique' do
     it 'rejects an instance creation attempt' do
-      puzzle_with_duplicate_title = @puzzle.dup
-      @puzzle.save
-      expect(puzzle_with_duplicate_title.title).to eq(@puzzle.title)
+      puzzle_with_duplicate_title = puzzle.dup
+      puzzle.save
+      expect(puzzle_with_duplicate_title.title).to eq(puzzle.title)
       expect(puzzle_with_duplicate_title).to_not be_valid
     end
   end
 
   context 'when creator_id and language_id is null' do
     it 'rejects an instance creation attempt' do
-      puzzle_without_creator_id_and_langauge_id = puzzle
-      expect(puzzle_without_creator_id_and_langauge_id.creator_id).to be_falsy
-      expect(puzzle_without_creator_id_and_langauge_id.language_id).to be_falsy
-      expect(puzzle_without_creator_id_and_langauge_id).to_not be_valid
+      puzzle.language_id = nil
+      puzzle.creator_id = nil
+      expect(puzzle.language_id).to be_falsy
+      expect(puzzle.creator_id).to be_falsy
+      expect(puzzle).to_not be_valid
     end
   end
 end

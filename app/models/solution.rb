0a763b117d729c358eaa7e090a8157afd8cabd22
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-31 02:44:39 +0800

    chore: delete previous iter of source_code_yields_expected_output

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -28,16 +28,6 @@ class Solution < ApplicationRecord
 
   private
 
-  # def source_code_yields_expected_output
-  #   return if Rails.env.test?
-  #
-  #   evaluation = Judge0::Client.evaluate_source_code(source_code:, language_id:)
-  #   return if evaluation[:status] == 200 && evaluation[:data]['stdout'] == puzzle.expected_output
-  #
-  #   error_message = "yielded the incorrect output. stdout: #{evaluation[:data]['stdout'] || 'nil'}. stderr: #{evaluation[:data]['stderr']}"
-  #   errors.add(:source_code, error_message)
-  # end
-
   def source_code_yields_expected_output
     return if Rails.env.test?
 

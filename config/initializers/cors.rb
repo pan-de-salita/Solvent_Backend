57c4873aea5fdd483ad6bfd7fda2874d51027aa0
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-08-02 15:00:41 +0800

    chore: edit cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # TODO: Add an origin that matches the domain of deployed client.
-    origins 'http://localhost:3000'
+    origins 'https://solvent-nfkw.onrender.com'
 
     resource '*',
              headers: :any,
